998e67221f1ea64d99c09d61e7c3e61f6ebc3aa5
VTRiot <nharata.on.r6@gmail.com>
2022-06-15 09:42:04 +0900

    220615 はやたす Youtube Python x Bitcoin売買 Lessun5

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+#Lessun5
 #ライブラリのインポート
 from pprint import pprint
 import requests
 
 #URLの設定
 BASE_URL = "https://coincheck.com" 
-url = BASE_URL + "/api/ticker"
-
+url = BASE_URL + "/api/trades"
+# ed_id = 221771450
+# st_id = ed_id + 10
 
 #情報を取得する
-r = requests.get(url)
-r = r.json()
-print(r["high"])
+params = {
+    "pair":"btc_jpy",
+    # "ending_before":ed_id,
+    # "starting_after":st_id
+    "limit":100
+    
+}
+r = requests.get(url,params=params).json()
+#r = r.json()
+pprint(r)
