0be929e9d93863ed099ffd275aa74676ef8f7027
VTRiot <nharata.on.r6@gmail.com>
2022-06-15 15:55:55 +0900

    220615 レッスン８ 認証して残高を取得

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
-#Lessun7
-
+# Lessun8
+# ライブラリのインポート
 import configparser
+import hmac
+import hashlib
+from pprint import pprint
+import time
+
+import requests
+
 conf = configparser.ConfigParser()
 conf.read("config.ini")
 
 ACCESS_KEY = conf["coincheck"]["access_key"]
 SERCRET_KEY = conf["coincheck"]["sercret_key"]
 
-print(ACCESS_KEY)
-print(SERCRET_KEY)
+# URLの設定
+BASE_URL = "https://coincheck.com"
+url = BASE_URL + "/api/accounts/balance"
 
-# #ライブラリのインポート
-# from pprint import pprint
-# import requests
+nonce = str(int(time.time()))
 
-# #URLの設定
-# BASE_URL = "https://coincheck.com" 
-# url = BASE_URL + "/api/order_books"
+body = ""
+message = nonce + url + body
 
-# #情報を取得する
+signature = hmac.new(SERCRET_KEY.encode(),
+                     message.encode(),
+                     hashlib.sha256).hexdigest()
+
+headers = {
+    "ACCESS-KEY": ACCESS_KEY,
+    "ACCESS-NONCE": nonce,
+    "ACCESS-SIGNATURE": signature,
+    "Content-Type": "application/json"
+}
+
+# 情報を取得する
 # params = {
-#     "limit":5   
+#     "limit": 5
 # }
 
-# r = requests.get(url,params=params)
-# r = r.json()
+# r = requests.get(url, params=params)
+r = requests.get(url, headers=headers)
+r = r.json()
 # pprint(r["asks"][::-1])
 # pprint(r["bids"])
+pprint(r)
